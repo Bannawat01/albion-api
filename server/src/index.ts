e56221bde558229f85c8686a0e0706610891b896
Peera-27 <crbrsonline@gmail.com>
2025-07-28 23:27:45 +0700

    feat: update index.ts to use await for database connection and add server initialization feat: add user schema model for MongoDB with username, password, email, and createdAt fields chore: add SSL certificate and private key for secure connections

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,14 @@
+import Elysia from "elysia";
 import { connectToDatabase } from "./configs/database"
 
 console.log("Hello via Bun suiiii!")
-connectToDatabase.connect()
+await connectToDatabase.connect()
+
+const app = new Elysia()
+.listen({
+    port: Bun.env.PORT || 8800
+  })
+
+let protocol = 'http'
+
+console.log(`${protocol}://${app.server?.hostname}:${app.server?.port}`)
