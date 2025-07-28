e0ece1c3914e61bcd9a288b84b2401bf97981382
Peera-27 <crbrsonline@gmail.com>
2025-07-28 23:35:00 +0700

    fix localhost can not use 8080😒

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,18 @@
 import Elysia from "elysia";
 import { connectToDatabase } from "./configs/database"
+import { tlsConfig } from "./configs/tls";
 
 console.log("Hello via Bun suiiii!")
 await connectToDatabase.connect()
 
 const app = new Elysia()
 .listen({
-    port: Bun.env.PORT || 8800
+    port: Bun.env.PORT || 8000,
+        tls: tlsConfig
+
   })
 
 let protocol = 'http'
-
+if ('cert' in tlsConfig)
+  protocol = 'https'
 console.log(`${protocol}://${app.server?.hostname}:${app.server?.port}`)
