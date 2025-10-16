94e70977d60cbdd278ebe67375ac43cb2e03bf64
Bannawat <109212993+Bannawat01@users.noreply.github.com>
2025-10-17 03:43:55 +0700

    fix: Update API base URL in config and Vercel configuration to use the correct backend endpoint

--- client/api/config.ts
+++ client/api/config.ts
@@ -1,6 +1,6 @@
 import { handleApiError } from '@/lib/errorMessage'
 import axios from 'axios'
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "https://albion-api-6o2m.onrender.com"
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "https://albion-backend-4exf.onrender.com"
 export const API_PREFIX = "/api"
 
 export const axiosInstance = axios.create({
